3fb844048ac9603fd2d5c14bb996cecfae63ac67
Rasib Hassan <mbs128rasib@gmail.com>
2023-06-23 16:05:42 +0500

    last

--- mysys/urls.py
+++ mysys/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
-
+admin.site.site_header= "Smart College Database"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('studentmanagementsystem.urls'))
